70539225da0d89c3dbd74c211a496aa4c858e714
gymnastiar32 <gymnastiar32@gmail.com>
2022-06-24 17:36:47 +0700

    first commit

---
+++ src/component/App.js
@@ -0,0 +1,12 @@
+import '../assets/css/App.css';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
